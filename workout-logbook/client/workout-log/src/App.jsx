046c5db40510f4b4c1b24d491f1cbf2876c2833c
Jake Greene <jsgreene208@gmail.com.com>
2021-08-23 07:52:32 -0400

    Work in Progress build of client side workout log

--- workout-logbook/client/workout-log/src/App.jsx
+++ workout-logbook/client/workout-log/src/App.jsx
@@ -1,15 +1,41 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect, Component } from 'react'
+import Auth from './auth/Auth';
 import Sitebar from './home/Navbar';
+import WorkoutIndex from './workouts/WorkoutIndex'
+
 
 function App() {
 
-  useEffect(() => {
-    document.title = "Workout Log Client"
+  const [sessionToken, setSessionToken] = useState(''); //1
+
+
+
+  useEffect(() =>{ //2
+    if(localStorage.getItem('token')){
+      setSessionToken(localStorage.getItem('token'))
+    }
   }, [])
+
+  const updateToken = (newToken) => { //3
+  localStorage.setItem('token', newToken);
+  setSessionToken(newToken);
+  console.log(sessionToken);
+  }
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken('');
+  }
+
+  const protectedViews = () => {
+    return ( sessionToken === localStorage.getItem('token') ? <WorkoutIndex token ={sessionToken} />
+    : <Auth updateToken={updateToken} /> );
+  }
 
   return (
     <div>
-      <Sitebar />
+      <Sitebar clickLogout={clearToken} />
+      {protectedViews()}
     </div>
   );
 }
